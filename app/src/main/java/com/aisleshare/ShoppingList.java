617032bbecb0bede2da6a6c974a8f3d29dab11c7
dlacroix1 <dmlacroix@tamu.edu>
2015-10-06 22:21:56 -0500

    Changed Structure of New List Popup
    Fixes formatting issue previously discussed. No longer uses
    "addlistmenu.xml", but uses AlertDialog. Still need to figure out how to
    open keyboard immediately.

--- app/src/main/java/com/aisleshare/ShoppingList.java
+++ app/src/main/java/com/aisleshare/ShoppingList.java
@@ -3,18 +3,23 @@ package com.aisleshare;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
@@ -22,6 +27,7 @@ import com.getbase.floatingactionbutton.FloatingActionButton;
 
 
 public class ShoppingList extends ActionBarActivity {
+    public final static String LIST_NAME = "com.ShoppingList.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +36,36 @@ public class ShoppingList extends ActionBarActivity {
 
         FloatingActionButton b = (FloatingActionButton) findViewById(R.id.float_button);
 
-        b.setOnClickListener(new View.OnClickListener(){
-
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ShoppingList.this, AddListMenu.class));
+                //startActivity(new Intent(ShoppingList.this, AddListMenu.class));
+
+                AlertDialog modal = new AlertDialog.Builder(ShoppingList.this).create();
+                modal.setTitle("Add a New List");
+
+                // Set up the input
+                final EditText input = new EditText(ShoppingList.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                input.setSingleLine(true);
+                modal.setView(input);
+
+                // Positive Button
+                modal.setButton(-1, "Add", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(ShoppingList.this, CurrentList.class);
+                        intent.putExtra(LIST_NAME, input.getText().toString());
+                        startActivity(intent);
+                    }
+                });
+
+                // Negative Button
+                modal.setButton(-2, "Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which){
+                    }
+                });
+
+                modal.show();
             }
         });
     }
